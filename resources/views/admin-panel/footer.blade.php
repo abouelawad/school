91626e842374225c3ee9857688acaec99146418f
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2024-05-06 18:43:07 +0300

    making the theme on

---
+++ resources/views/admin-panel/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-arrow-circle-up"></span></div>
+
+ 
+
+<script src="{{ asset('assets') }}/js/jquery.js"></script>
+<script src="{{ asset('assets') }}/js/popper.min.js"></script>
+<script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets') }}/js/jquery.fancybox.js"></script>
+<script src="{{ asset('assets') }}/js/appear.js"></script>
+<script src="{{ asset('assets') }}/js/isotope.js"></script>
+<script src="{{ asset('assets') }}/js/owl.js"></script>
+<script src="{{ asset('assets') }}/js/wow.js"></script>
+<script src="{{ asset('assets') }}/js/jquery-ui.js"></script>
+<script src="{{ asset('assets') }}/js/script.js"></script>
+<script src="{{ asset('assets') }}/js/color-settings.js"></script>
+
+</body>
+
+</html>
