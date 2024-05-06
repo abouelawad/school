40bd58a59aeeb13fba5dc8c0ba284412b0bd6a54
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2024-05-06 18:43:07 +0300

    making the theme on

---
+++ resources/views/admin-panel/styles.blade.php
@@ -0,0 +1,28 @@
+<!-- Stylesheets -->
+<link href="{{ asset('assets') }}/css/bootstrap.css" rel="stylesheet">
+<link href="{{ asset('assets') }}/css/main.css" rel="stylesheet">
+<link href="{{ asset('assets') }}/css/responsive.css" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&amp;family=Roboto:wght@300;400;500;700;900&amp;display=swap" rel="stylesheet">
+
+<!-- Color Switcher Mockup -->
+<link href="{{ asset('assets') }}/css/color-switcher-design.css" rel="stylesheet">
+<!-- Color Themes -->
+<link id="theme-color-file" href="{{ asset('assets') }}/css/color-themes/theme-one.css" rel="stylesheet">
+
+<link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="{{ asset('assets') }}/images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="{{ asset('assets') }}/js/respond.js"></script><![endif]-->
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&family=Tajawal:wght@500;700&display=swap" rel="stylesheet">
+<style>
+	h1 , h2 , h3 , h4 , h5 , h6 , p , div , ul , li a , input , button {
+		font-family: 'Cairo', sans-serif !important;
+		line-height: 1.7; 
+	}
+</style>
